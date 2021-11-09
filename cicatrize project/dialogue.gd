4a92ceeb4db5b1496d9aae42ab8e98ad0d3288c3
ayamerp <hayilaaiv@gmail.com>
2021-11-10 01:48:13 +0800

    did starting first dialogue

---
+++ cicatrize project/dialogue.gd
@@ -0,0 +1,39 @@
+extends Node
+
+var dialogue1 = [
+	{
+		"Name": "Elfa",
+		"Expression": "Neutral",
+		"Position": "1",
+		"Choices": ["", ""],
+		"Text": "Mr. Klaus, the new elf is now ready for work.",
+	},
+	{
+		"Name": "Santa",
+		"Expression": "Neutral",
+		"Position": "1",
+		"Choices": ["", ""],
+		"Text": "Throw him away.",
+	},
+	{
+		"Name": "Elfa",
+		"Expression": "Neutral",
+		"Position": "1",
+		"Choices": ["", ""],
+		"Text": "W-what do you mean sir?",
+	},
+	{
+		"Name": "Santa",
+		"Expression": "Neutral",
+		"Position": "1",
+		"Choices": ["", ""],
+		"Text": "He turned out to be a defect so throw him away. Do I have to repoeat myself?",
+	},
+	{
+		"Name": "Elfa",
+		"Expression": "Neutral",
+		"Position": "1",
+		"Choices": ["", ""],
+		"Text": "N-no sir. I will do what you ask now.",
+	},
+]
